51ab88afc6edc89bae28c0b6618f9fb88ae4070b
Marine Mignot <mignomarine@gmail.com>
2025-03-13 19:59:52 +0100

    app

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router(); // Crée une instance de routeur
-const User = require('../models/User'); // Import du modèle utilisateur
+const User = require('../models/user'); // Import du modèle utilisateur
 
 // Middleware spécifique (si nécessaire, sinon déléguez-le au fichier principal)
 const bcrypt = require('bcrypt');
