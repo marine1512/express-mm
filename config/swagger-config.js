db1f84431ecd138a2b3297237a78932b45af92ed
Marine Mignot <mignomarine@gmail.com>
2025-04-14 18:10:49 +0200

    app

--- config/swagger-config.js
+++ config/swagger-config.js
@@ -1,17 +1,16 @@
 const swaggerJsDoc = require('swagger-jsdoc');
 
 const swaggerOptions = {
-    definition: {
-      openapi: '3.0.0',
-      info: {
-        title: 'API Documentation',
-        version: '1.0.0',
-        description: 'Documentation de mon API'
-      }
-    },
-    apis: ['./routes/*.js'], // Chemin où Swagger analyse vos routes
-  };
-  
-  const swaggerDocs = swaggerJsDoc(swaggerOptions);
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API Documentation',
+      version: '1.0.0',
+      description: 'Documentation de mon API'
+    }
+  },
+  apis: ['./routes/*.js'], // Analyser toutes les routes dans le dossier `routes/`
+};
 
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
 module.exports = swaggerDocs;
